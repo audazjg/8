dc61d6d54910cf357de7f816a503010bea126316
audazjg <audazjg@gmail.com>
2024-12-16 12:51:13 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 10000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 10000,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
+});
